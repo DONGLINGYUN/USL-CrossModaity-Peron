a5b79313315b31a54ec35ad9970edcbb0f150e05
Dly <59408667+Dongly502@users.noreply.github.com>
2024-04-24 10:56:49 +0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -3,4 +3,4 @@ do
 CUDA_VISIBLE_DEVICES=0 python label_regdb4.py -b 256 -a agw -d  regdb_rgb --iters 100 --momentum 0.1 --eps 0.6 --num-instances 16 --trial $trial
 done
 echo 'Do
-python train_regdb.py/train_regdb_baseline.py -b 32 -a agw -d  regdb_rgb --iters 100 --momentum 0.1 --eps 0.3 --num-instances 16
+python train.py/train_baseline.py -b 32 -a agw -d  regdb_rgb --iters 100 --momentum 0.1 --eps 0.3 --num-instances 16
